533e1e6e0ca0b13bc92fa10b1abd368666e5831f
Pranv Upadhyay <upadhyaypranav89>
2023-05-01 11:02:00 +0530

    JavaScript

---
+++ index.js
@@ -0,0 +1,31 @@
+//console.log("hello word")
+//var myName="Pranav Upadhyay"
+//console.log(myName)
+
+//Data Types practice
+
+//console.log(10+"20");
+//console.log("java"+"Script");
+//console.log(10-"20"); //bug1
+//console.log(true+false); //true=1 , false=0
+
+//var iAmuseless = null;
+//console.log(iAmuseless);
+//console.log(typeof(iAmuseless));//Bug 2
+
+//console.log("java" - "Script")//NaN -> Not a number
+// what is NaN?
+//-> NaN is a property of the global object.
+// it is a variable in global scope. 
+
+//NaN practice
+
+//NaN === NaN; -> False
+//Number.NaN === NaN; -> False
+//isNaN(NaN); ->true
+//isNaN(Number.log); ->true
+//Number.isNaN(NaN); ->true
+
+var a= 30;
+var b =10;
+console.log(a!==b);
