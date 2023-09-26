44e6cf29d2b29d0050460440a485583877b5a5dd
Pranav_Upadhyay <105533894+Pranavbeta@users.noreply.github.com>
2023-09-26 09:44:49 +0000

    01_varibles

---
+++ 01_Basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 14423
+let accountEmail = "pranavupadhyay34.com"
+var accountPassword = "1234"
+accountcity="jaipur"
+
+
+// prefer do not use var 
+  
+//accountId= 2 // not allowed
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountcity]);
